20268cb28ff26acc584d58423af6cdbc274718f9
Urameshx <88640471+Urameshx@users.noreply.github.com>
2021-08-19 17:44:26 -0300

    Update ED_Ex2_Recursividade.java

--- src/controller/ED_Ex2_Recursividade.java
+++ src/controller/ED_Ex2_Recursividade.java
@@ -6,9 +6,17 @@ public class ED_Ex2_Recursividade {
 	}
 
 	public int fatorial (int num) {
+		/*Condição de parada = Por se tratar de número fatorial, 
+		será qualquer decremento do número inicial igual a 1*/
+		
 		if (num == 1) {
 			return 1;
-		} else {
+		} 
+		/* O valor da entrada será multiplicado por ela própria, após isso 
+		será subtraído um número. O processo se repetirá até o valor da entrada ser igual a 1.
+		*/
+		
+		else {
 			return num * fatorial (num - 1);
 		}
 	}
